7ef54efc58f834da406c7219bf92e1ef234e2ba0
makarimachmad <makarimachmad@gmail.com>
2018-07-20 16:59:08 +0700

    login berhasil atau gagal -makarim

---
+++ app/src/main/java/com/example/witono/jogjaflight/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.witono.jogjaflight;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.logojakarta);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void tombolabsensi(View view){
+        Intent intents = new Intent(MainActivity.this,AbsensiActivity.class);
+        startActivity(intents);
+    }
+    public void tomboljadwal(View view){
+        Intent intents = new Intent(MainActivity.this,JadwalActivity.class);
+        startActivity(intents);
+    }
+    public void tombolnilai(View view){
+        Intent intents = new Intent(MainActivity.this,NilaiActivity.class);
+        startActivity(intents);
+    }
+    public void tombolmatakuliah(View view){
+        Intent intents = new Intent(MainActivity.this,MataKuliahActivity.class);
+        startActivity(intents);
+    }
+}
--- app/src/main/java/com/example/witono/jogjaflight/NilaiActivity.java
+++ app/src/main/java/com/example/witono/jogjaflight/NilaiActivity.java
@@ -1,13 +1,13 @@
 package com.example.witono.jogjaflight;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class Main2Activity extends AppCompatActivity {
+public class NilaiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.nilai_layout);
     }
 }
